439693ebf225c8ae08fdc8a45183b1609b27c978
JheovannyCampos <jheovanny06@hotmail.com>
2021-07-04 00:59:50 -0300

    ChessException and ChessPosition

--- boardgame/BoardException.java
+++ boardgame/BoardException.java
@@ -3,7 +3,7 @@ package boardgame;
 public class BoardException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public  BoardException(String msg) {
+    public BoardException(String msg) {
         super(msg);
     }
     }
